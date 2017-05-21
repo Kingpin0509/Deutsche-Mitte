500aeea8c3b6afda2f8eff8b5f7c06f458a4f99e
Kingpin0509 <Kingpin0509@gmail.com>
2017-05-22 08:54:47 +0900

    123

---
+++ www/scripts/customAnimator.ts
@@ -0,0 +1,185 @@
+/**
+ * @Kingpin0509 - Michael Humann - West Weedtze Development
+ * Copyright (c) 2017 (ae)Deutsche Mitte - http://deutschemitte.daskonstrukt.xyz
+ * Custom Changes an der onsenui v2.1.0 - 2017-02-01
+ */
+// CUSTOM ANIMATOR
+ 
+(function() {
+    'use strict;';
+    var module = angular.module('onsen');
+    module.factory('customDropAnimation', function(NavigatorTransitionAnimator) {
+        var customDropAnimation = NavigatorTransitionAnimator.extend({
+            backgroundMask: angular.element(
+                '<div style="position: absolute; width: 100%;' +
+                'height: 100%; background-color: #ffffff;"></div>'
+            ),
+            push: function(enterPage, leavePage, callback) {
+                var mask = this.backgroundMask.remove();
+                leavePage.element[0].parentNode.insertBefore(mask[0], leavePage.element[0]);
+                var maskClear = animit(mask[0])
+                    .wait(0.6)
+                    .queue(function(done) {
+                        mask.remove();
+                        done();
+                    });
+                animit.runAll(
+                    maskClear,
+                    animit(enterPage.element[0])
+                    .queue({
+                        css: {
+                            transform: 'translate3D(0, -100%, 0)',
+                        },
+                        duration: 0
+                    })
+                    .queue({
+                        css: {
+                            transform: 'translate3D(0, 0, 0)',
+                        },
+                        duration: 0.4,
+                        timing: 'cubic-bezier(.47,0,.47,1)'
+                    })
+                    // .wait(0.2) -> causes flicker sometimes
+                    .resetStyle()
+                    .queue(function(done) {
+                        callback();
+                        done();
+                    }),
+                    // dont move leaving page
+                    animit(leavePage.element[0])
+                );
+            },
+            pop: function(enterPage, leavePage, callback) {
+                var mask = this.backgroundMask.remove();
+                enterPage.element[0].parentNode.insertBefore(mask[0], enterPage.element[0]);
+                animit.runAll(
+                    animit(mask[0])
+                    .wait(0.4)
+                    .queue(function(done) {
+                        mask.remove();
+                        done();
+                    }),
+                    // dont move entering page
+                    animit(enterPage.element[0]),
+                    animit(leavePage.element[0])
+                    .queue({
+                        css: {
+                            transform: 'translate3D(0, 0, 0)'
+                        },
+                        duration: 0
+                    })
+                    .queue({
+                        css: {
+                            transform: 'translate3D(0, -100%, 0)'
+                        },
+                        duration: 0.4,
+                        timing: 'cubic-bezier(.1, .7, .1, 1)'
+                    })
+                    .wait(0.2)
+                    .resetStyle()
+                    .queue(function(done) {
+                        callback();
+                        done();
+                    })
+                );
+            }
+        });
+        return customDropAnimation;
+    });
+})();
+
+//
+//var customAnimator = function(options) {
+//    options = options || {};
+//    this.timing = options.timing || 'ease';
+//    this.duration = options.duration || 0.35;
+//    this.delay = options.delay || 0.0;
+//    this.delay0 = options.delay || 0.0;
+//    this.delay1 = options.delay || 0.00;
+//
+//    var div = document.createElement('div');
+//    div.innerHTML = '<div style="position: absolute; width: 100%; height: 100%; background-color: black; z-index: 2; background-image: url(../images/pictures_vertical/bg1.jpg) cover; opacity: 1;"></div>';
+//    this.backgroundMask = div.firstChild;
+//    this.blackMaskOpacity = 0.95;
+//};
+//
+//customAnimator.prototype = Object.create(ons.NavigatorElement.NavigatorTransitionAnimator.prototype);
+//
+//customAnimator.prototype.push = function(enterPage, leavePage, done) {
+//    this.backgroundMask.remove();
+//    enterPage.parentNode.insertBefore(this.backgroundMask, enterPage.nextSibling);
+//
+//    ons.animit.runAll(
+//
+//        ons.animit(this.backgroundMask)
+//        .saveStyle()
+//        .queue({
+//            opacity: this.blackMaskOpacity,
+//            transform: 'translate3d(0, 0, 0)'
+//        })
+//        .wait(this.delay)
+//        .queue({
+//            opacity: 0.15
+//        }, {
+//            duration: this.duration,
+//            timing: this.timing
+//        })
+//        .restoreStyle()
+//        .queue(done => {
+//            this.backgroundMask.remove();
+//            done();
+//        }),
+//        ons.animit(enterPage)
+//        .saveStyle()
+//        .queue({
+//            css: {
+//                transform: 'translate3D(0%, 220px, 0px)',
+//                height: '5%',
+//                width: '100%',
+//                opacity: 0.5
+//            },
+//            duration: 1
+//        })
+//        .wait(this.delay1)
+//        .queue({
+//            css: {
+//                transform: 'translate3D(0px, 0px, 0px)',
+//                height: '100%',
+//                width: '100%',
+//                opacity: 1
+//            },
+//            duration: this.duration,
+//            timing: this.timing
+//        })
+//        .restoreStyle(),
+//        ons.animit(leavePage)
+//        .queue({
+//            css: {
+//                transform: 'translate3D(0px, 0px, 0px)',
+//                height: '100%',
+//                width: '100%',
+//                display: 'block',
+//                opacity: 0.9
+//            },
+//            duration: 1
+//        })
+//        .wait(this.delay0)
+//        .queue({
+//            css: {
+//                opacity: 0.1,
+//                height: '4%',
+//                width: '100%',
+//                transform: 'translate3D(0%,230px, 0px)',
+//            },
+//            duration: this.duration,
+//            timing: this.timing
+//        })
+//        .wait(0.1)
+//        .queue(function(finish) {
+//            done();
+//            finish();
+//        })
+//    );
+//};
+//
+//ons.NavigatorElement.registerAnimator('customAnimator', customAnimator);
--- www/scripts/window.fn.ts
+++ www/scripts/window.fn.ts
@@ -1,20 +1,21 @@
+// Window.fn
 window.fn = {};
-window.fn.open = function () {
+window.fn.open = function() {
     var menu = document.getElementById('menu');
     menu.open();
 };
-window.fn.openr = function () {
+window.fn.openr = function() {
     var menur = document.getElementById('menur');
     menur.open();
 };
-window.fn.close = function () {
+window.fn.close = function() {
     var menu = document.getElementById('menu');
     menu.close();
 };
-window.fn.closesplitter = function () {
+window.fn.closesplitter = function() {
     splitter.right.close();
 };
-window.fn.load = function (page) {
+window.fn.load = function(page) {
     var navi = document.getElementById('navi');
     navi.resetToPage(page, {
         animation: 'fade'
@@ -22,58 +23,58 @@ window.fn.load = function (page) {
     splitter.left.close();
     splitter.right.close();
 };
-window.fn.push = function (page) {
+window.fn.push = function(page) {
     var navi = document.getElementById('navi');
     navi.pushPage(page, {
         animation: 'slide'
     });
 };
-window.fn.replace = function (page) {
+window.fn.replace = function(page) {
     var navi = document.getElementById('navi');
     navi.replacePage(page, {
         animation: 'fade'
     });
 };
-
-window.fn.toggle = function (el) {
+// Collapsible Accordion customDropAnimation
+window.fn.toggle = function(el) {
     el.classList.toggle("active-item");
     el.nextElementSibling.classList.toggle("show");
 };
-
-window.fn.togglemenu = function (el) {
+// Menu Accordion
+window.fn.togglemenu = function(el) {
     var btns = document.querySelectorAll(".panel");
-    Array.prototype.forEach.call(btns, function (el) {
+    Array.prototype.forEach.call(btns, function(el) {
         el.classList.remove("show");
     });
     el.nextElementSibling.classList.toggle("show");
 };
-window.fn.togglepanels = function (el) {
+window.fn.togglepanels = function(el) {
     var btns = document.querySelectorAll(".panel");
-    Array.prototype.forEach.call(btns, function (el) {
+    Array.prototype.forEach.call(btns, function(el) {
         el.classList.remove("show");
     });
 };
-window.fn.active = function (el) {
+window.fn.active = function(el) {
     var btns = document.querySelectorAll('.active-item');
-    Array.prototype.forEach.call(btns, function (el) {
+    Array.prototype.forEach.call(btns, function(el) {
         el.classList.remove('active-item');
     });
     var btns1 = document.querySelectorAll('panel');
-    Array.prototype.forEach.call(btns1, function (el) {
+    Array.prototype.forEach.call(btns1, function(el) {
         el.classList.remove('show');
     });
     el.classList.add('active-item');
 };
-window.fn.activemenu = function (el) {
+window.fn.activemenu = function(el) {
     var btns = document.querySelectorAll("ons-list-header.list-header");
-    Array.prototype.forEach.call(btns, function (el) {
+    Array.prototype.forEach.call(btns, function(el) {
         el.classList.remove("active-item");
     });
     el.classList.add('active-item');
 };
-window.fn.activesub = function (el) {
+window.fn.activesub = function(el) {
     var btns = document.querySelectorAll(".active-item");
-    Array.prototype.forEach.call(btns, function (el) {
+    Array.prototype.forEach.call(btns, function(el) {
         el.classList.remove("active-item");
     });
     el.classList.add("active-item");
