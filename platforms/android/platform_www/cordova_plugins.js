285add97a3c9007955c6590a0f9ff257a07e6b97
Michael Humann <Kingpin0509@gmail.com>
2017-11-02 06:20:54 +0100

    0620

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -2,9 +2,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-  "cordova-plugin-ios-no-export-compliance": "0.0.2",
-  "cordova-ios-plugin-no-export-compliance": "0.0.5"
-};
+{};
 // BOTTOM OF METADATA
 });
